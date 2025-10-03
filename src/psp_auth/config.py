c24fd8df31a36c21c407f18f98ae7f6173193b60
Jonas Bork <jonas10bork@gmail.com>
2025-10-03 13:35:01 +0200

    Made AuthConfig into a dataclass and fixed default well-known location

--- src/psp_auth/config.py
+++ src/psp_auth/config.py
@@ -1,6 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class AuthConfig:
-    well_known_endpoint: str = "http://user.psp.svc.cluster.local/.well-known/intra"
+    well_known_endpoint: str = (
+        "http://user.psp.svc.cluster.local/.well-known/openid-configuration/internal"
+    )
     request_timeout: tuple[int, int] = (1, 5)
-    client_id: str = ""
+    client_id: str = "external"
     client_secret: str = ""
     _client_name: str = "psp"

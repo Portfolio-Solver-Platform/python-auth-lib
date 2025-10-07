cd1b8109e7d01102864a807560a015d5a39246a5
Jonas Bork <jonas10bork@gmail.com>
2025-10-07 13:43:06 +0200

    Implemented token decoding

--- src/psp_auth/core.py
+++ src/psp_auth/core.py
@@ -1,13 +1,21 @@
 from starlette.applications import Starlette
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
+from joserfc import jwt
 
 from .config import AuthConfig
+from .cache import CachedGetter
 from .endpoints import OidcEndpoints
 from .logging import PrintLogger
 from .token import Token
 
 from authlib.integrations.starlette_client import OAuth
+
+
+def get_signing_cert(url: str) -> dict:
+    certs = requests.get(url).json()
+    signing_certs = filter(lambda key: key.use == "sig", certs["keys"])
+    return signing_certs[0]
 
 
 class Auth:
@@ -18,6 +26,8 @@ class Auth:
     config: AuthConfig
     logger: any
     oauth: OAuth
+    _endpoints: CachedGetter
+    _certs: CachedGetter
 
     def __init__(self, config: AuthConfig | None = None, logger: any = None):
         """
@@ -25,6 +35,10 @@ class Auth:
         """
         self.config = config if config is not None else AuthConfig()
         self.logger = logger if logger is not None else PrintLogger()
+        self._endpoints = OidcEndpoints(self.config.well_known_endpoint)
+        self._signing_cert = CachedGetter(
+            lambda: get_signing_cert(self._endpoints.signing_cert()), 60 * 60
+        )
 
         self.oauth = OAuth()
         self.oauth.register(
@@ -35,14 +49,36 @@ class Auth:
             client_kwargs={"scope": "openid email profile"},
         )
 
+    def certs(self) -> dict:
+        return requests.get(self._endpoints.certs()).json()
+
+    def signing_cert(self) -> dict:
+        return self._signing_cert.get()
+
     def enable(self, app: Starlette, secret_key: str) -> None:
         app.add_middleware(SessionMiddleware, secret_key=secret_key)
 
-    async def get_token(self, request: Request) -> Token:
+    async def get_token(self, request: Request) -> jwt.Token:
         """
         Authorizes the token locally and returns it.
         """
-        return await self.client().authorize_access_token(request)
+        token = self.get_unverified_token(request)
+        key_set = jwt.KeySet.import_key_set(self.certs())
+        return jwt.decode(token, key_set)
+
+    async def get_unverified_token(self, request: Request):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header:
+            raise HTTPException(status_code=401, detail="Missing Authorization header")
+
+        # Extract token from "Bearer <token>"
+        parts = auth_header.split()
+
+        if len(parts) != 2 or parts[0].lower() != "bearer":
+            raise HTTPException(status_code=401, detail="Invalid Authorization header")
+
+        return parts[1]
 
     async def get_verified_token(self, request: Request) -> Token:
         """
--- src/psp_auth/endpoints.py
+++ src/psp_auth/endpoints.py
@@ -1,9 +1,30 @@
+from .cache import CachedGetter
+import requests
+
+
+def _request_metadata(url: str) -> dict:
+    response = requests.get(url)
+    return response.json()
+
+
 class OidcEndpoints:
+    _metadata_response: CachedGetter
     token: str
     introspection: str
     userinfo: str
     end_session: str
     jwks_uri: str
+
+    def __init__(self, server_metadata_url: str):
+        self._metadata_response = CachedGetter(
+            lambda: _request_metadata(server_metadata_url), 60 * 60
+        )
+
+    def update(self):
+        self._metadata_response.update()
+
+    def certs(self) -> dict:
+        return self._metadata_response["jwks_uri"]
 
     def set_from_well_known(self, well_known: any):
         self.token = well_known["token_endpoint"]
